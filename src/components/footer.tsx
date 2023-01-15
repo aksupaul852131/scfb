cd780de2ed25224892dfa4ddea0fc96ef530d3fd
aksupaul852131 <aksupaul.in@gmail.com>
2023-01-14 23:19:31 -0800

    first commit 2

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 const Footer = () => {
     return (
@@ -7,7 +8,7 @@ const Footer = () => {
             <div className="mx-auto max-w-7xl pt-16 font-['Poppins']">
                 <footer className="p-4  rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
                     <div className="sm:flex sm:items-center sm:justify-between">
-                        <a href="/" className="flex items-center mb-4 sm:mb-0">
+                        <Link href="/" className="flex items-center mb-4 sm:mb-0">
                             <img
                                 src="/logo1.png"
                                 className="h-8 mr-3"
@@ -16,36 +17,36 @@ const Footer = () => {
                             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                                 Smart Care
                             </span>
-                        </a>
+                        </Link >
                         <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
                             <li>
-                                <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                                <Link href="#" className="mr-4 hover:underline md:mr-6 ">
                                     About
-                                </a>
+                                </Link >
                             </li>
                             <li>
-                                <a href="#" className="mr-4 hover:underline md:mr-6">
+                                <Link href="#" className="mr-4 hover:underline md:mr-6">
                                     Privacy Policy
-                                </a>
+                                </Link >
                             </li>
                             <li>
-                                <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                                <Link href="#" className="mr-4 hover:underline md:mr-6 ">
                                     Licensing
-                                </a>
+                                </Link >
                             </li>
                             <li>
-                                <a href="#" className="hover:underline">
+                                <Link href="#" className="hover:underline">
                                     Contact
-                                </a>
+                                </Link >
                             </li>
                         </ul>
                     </div>
                     <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                     <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
                         ©{" "}
-                        <a href="/" className="hover:underline">
+                        <Link href="/" className="hover:underline">
                             SmartCare™
-                        </a>
+                        </Link >
                         . All Rights Reserved.
                     </span>
                 </footer>
