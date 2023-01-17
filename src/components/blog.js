d7e1f51cd8e757f8d850c06aa512e7546f3d3e47
aksupaul852131 <aksupaul.in@gmail.com>
2023-01-17 03:46:09 -0800

    nav fixed v2

--- src/components/blog.js
+++ src/components/blog.js
@@ -11,52 +11,80 @@ function Blog({ blogs }) {
     }
     return (
 
-        <div className="bg-[#efefef]">
+        <div className="bg-[#fffdfd]">
+
+            <section className="text-gray-600 body-font">
+                <div className="container max-w-7xl pb-8 px-0 lg:px-4 mx-auto">
+                    <h2 className="text-2xl md:text-4xl font-semibold text-center">Blogs & Updates</h2>
+                    <div className="flex flex-wrap">
+                        {!blogs.length > 0 && <p>No Blogs Post</p>}
+                        {blogs.length > 0 && (
+
+                            blogs.map((b) => (
+
+                                <div className="p-4 md:w-1/3">
+                                    <div className="h-full border-2 bg-white border-gray-200 border-opacity-60 rounded-2xl overflow-hidden">
+                                        <img
+                                            className="lg:h-48 md:h-36 w-full object-cover object-center"
+                                            src={urlFor(b.mainImage).width(600).url()}
+                                            alt="blog"
+                                        />
+                                        <div className="p-6">
+                                            <span className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                                                Blog
+                                            </span>
+                                            <h2 className="title-font text-lg font-medium text-gray-900 mb-3">
+                                                <Link href={`/blog/${b.slug.current}`}>{b.title}</Link>
+                                            </h2>
+                                            <p className="leading-relaxed mb-3">
+                                                {b.metadesc}
+                                            </p>
+                                            <div className="flex items-center flex-wrap ">
+                                                <Link href={`/blog/${b.slug.current}`} className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                                                    Read More
+                                                    <svg
+                                                        className="w-4 h-4 ml-2"
+                                                        viewBox="0 0 24 24"
+                                                        stroke="currentColor"
+                                                        strokeWidth={2}
+                                                        fill="none"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                    >
+                                                        <path d="M5 12h14" />
+                                                        <path d="M12 5l7 7-7 7" />
+                                                    </svg>
+                                                </Link>
+                                                <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 ">
 
 
-
-            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 py-14 md:py-32 font-['Poppins']">
-                <div className="mb-12 space-y-2 text-center">
-                    <h2 className="text-2xl md:text-4xl font-bold text-center">
-                        Blog & Care
-                    </h2>
-
-                </div>
-                <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-
-
-                    {!blogs.length > 0 && <p>No Blogs Post</p>}
-                    {blogs.length > 0 && (
-
-                        blogs.map((b) => (
-
-                            <div key={b._id} className="group p-6 sm:p-8 rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
-                                <div className="relative overflow-hidden rounded-xl">
-                                    <img
-                                        src={urlFor(b.mainImage).width(600).url()}
-                                        alt="art cover"
-                                        loading="lazy"
-                                        width={1000}
-                                        height={667}
-                                        className="md:h-40 h-54 bg-gray-200 rounded-lg w-full object-cover object-top transition duration-500 group-hover:scale-105"
-                                    />
+                                                    <svg
+                                                        className="w-4 h-4 mr-1"
+                                                        stroke="currentColor"
+                                                        strokeWidth={2}
+                                                        fill="none"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        viewBox="0 0 24 24"
+                                                    >
+                                                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+                                                    </svg>
+                                                    1
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="mt-6 relative">
-                                    <h3 className="text-1xl font-semibold text-gray-900 dark:text-white">
-                                        <Link href={`/blog/${b.slug.current}`}>{b.title}</Link>
-                                    </h3>
 
 
-                                </div>
-                            </div>
-                        ))
+                            ))
 
-                    )}
-
-
-
+                        )}
+                    </div>
                 </div>
-            </div>
+            </section>
+
+
 
             <div className="bg-white">
                 <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 pt-16 pb-24 font-['Poppins']">
--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -207,38 +207,72 @@ const Slug = ({ post, relatedPosts }) => {
                     <h2 className="mb-8 text-2xl font-bold text-gray-900 dark:text-white">
                         Related articles
                     </h2>
-                    <div className="grid gap-12 sm:grid-cols-2 lg:grid-cols-3">
-                        {!relatedPosts.length > 0 && <p>No pets to show</p>}
+
+                    <div className="flex flex-wrap">
+                        {!relatedPosts.length > 0 && <p>No Related Post</p>}
                         {relatedPosts.length > 0 && (
 
                             relatedPosts.map((b) => (
 
-                                <article key={b._id} className="w-full bg-gray-100 p-6 rounded-2xl">
-                                    <Link href={`/blog/${b.slug.current}`}>
+                                <div className="p-4 md:w-1/3">
+                                    <div className="h-full border-2 bg-white border-gray-200 border-opacity-60 rounded-2xl overflow-hidden">
                                         <img
+                                            className="lg:h-48 md:h-36 w-full object-cover object-center"
                                             src={urlFor(b.mainImage).width(600).url()}
-                                            alt={b.altText}
-                                            loading="lazy"
-                                            width={1000}
-                                            height={667}
-                                            className="w-full mb-5 rounded-lg"
-
+                                            alt="blog"
                                         />
-                                    </Link>
-                                    <h2 className="mb-2 hover:text-blue-700 text-xl font-bold leading-tight text-gray-900 dark:text-white">
-                                        <Link href={`/blog/${b.slug.current}`}>{b.title}</Link>
-                                    </h2>
+                                        <div className="p-6">
+                                            <span className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                                                Blog
+                                            </span>
+                                            <h2 className="title-font text-lg font-medium text-gray-900 mb-3">
+                                                <Link href={`/blog/${b.slug.current}`}>{b.title}</Link>
+                                            </h2>
+
+                                            <div className="flex items-center flex-wrap ">
+                                                <Link href={`/blog/${b.slug.current}`} className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                                                    Read More
+                                                    <svg
+                                                        className="w-4 h-4 ml-2"
+                                                        viewBox="0 0 24 24"
+                                                        stroke="currentColor"
+                                                        strokeWidth={2}
+                                                        fill="none"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                    >
+                                                        <path d="M5 12h14" />
+                                                        <path d="M12 5l7 7-7 7" />
+                                                    </svg>
+                                                </Link>
+                                                <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 ">
 
 
-                                </article>
+                                                    <svg
+                                                        className="w-4 h-4 mr-1"
+                                                        stroke="currentColor"
+                                                        strokeWidth={2}
+                                                        fill="none"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        viewBox="0 0 24 24"
+                                                    >
+                                                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+                                                    </svg>
+                                                    1
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
                             ))
 
                         )}
-
-
-
-
                     </div>
+
+
                 </div>
             </aside>
             <section className="bg-white dark:bg-gray-900">
