4e92f4b4c9ffed30ae4645fea65d8d589de26cd1
aksupaul852131 <aksupaul.in@gmail.com>
2023-01-17 02:46:03 -0800

    nav fixed

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -21,11 +21,13 @@ export default function Nav() {
                 <>
                     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                         <div className="relative flex h-16 items-center justify-between">
-                            <img
-                                className="ml-2 block h-8 w-auto lg:hidden"
-                                src="/logo1.png"
-                                alt="Smart Care"
-                            />
+                            <Link href="/">
+                                <img
+                                    className="ml-2 block h-8 w-auto lg:hidden"
+                                    src="/logo1.png"
+                                    alt="Smart Care"
+                                />
+                            </Link>
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex flex-shrink-0 items-center">
 
