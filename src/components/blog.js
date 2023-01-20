7cd94924d7a4cafa6a890eb5b8aae708ec2a9eb8
aksupaul852131 <aksupaul.in@gmail.com>
2023-01-21 00:28:41 +0530

    sre 2

--- src/components/blog.js
+++ src/components/blog.js
@@ -78,7 +78,7 @@ function Blog({ blogs }) {
                                     <div className="h-full w-1 bg-gray-200 pointer-events-none" />
                                 </div>
                                 <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-700 inline-flex items-center justify-center text-white relative z-10">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 ">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
                                     </svg>
 
@@ -97,7 +97,7 @@ function Blog({ blogs }) {
                                     <div className="h-full w-1 bg-gray-200 pointer-events-none" />
                                 </div>
                                 <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-700 inline-flex items-center justify-center text-white relative z-10">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 animate-bounce ">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                                     </svg>
                                 </div>
@@ -131,7 +131,7 @@ function Blog({ blogs }) {
                             </div>
                             <div className="flex relative pb-12">
                                 <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-                                    <div className="h-full w-1 bg-gray-200 pointer-events-none" />
+
                                 </div>
                                 <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-700 inline-flex items-center justify-center text-white relative z-10">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -148,25 +148,10 @@ function Blog({ blogs }) {
                                     </p>
                                 </div>
                             </div>
-                            <div className="flex relative">
-                                <div className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-700 inline-flex items-center justify-center text-white relative z-10">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-                                    </svg>
 
-                                </div>
-                                <div className="flex-grow pl-4">
-                                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
-                                        FINISH
-                                    </h2>
-                                    <p className="leading-relaxed">
-                                        You Can Rate Our Service & Recommend To Your Freinds, if SATISFIED.
-                                    </p>
-                                </div>
-                            </div>
                         </div>
 
-                        <div className="hidden lg:block bg-blue-700 bg-[url(https://us.123rf.com/450wm/mar1ar1/mar1ar11810/mar1ar1181000342/mar1ar1181000342.jpg?ver=6)] rounded-lg lg:w-3/5 md:w-full object-cover object-center md:mt-0 mt-12 grid content-center justify-items-center p-12">
+                        <div className="hidden lg:block bg-blue-700 bg-cover bg-[url(https://us.123rf.com/450wm/mar1ar1/mar1ar11810/mar1ar1181000342/mar1ar1181000342.jpg?ver=6)] rounded-lg lg:w-3/5 md:w-full object-cover object-center md:mt-0 mt-12 grid content-center justify-items-center p-12">
                             <h3 className="text-lg text-white">Smart Care Supaul</h3>
                             <h3 className="text-lg md:text-4xl font-bold text-white">Simple Steps</h3>
                             <p className="text-white mt-4 text-xs lg:txt-base">Customer Helpline Number - 8825105519</p>
