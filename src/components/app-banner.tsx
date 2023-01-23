a86d4349dd18fa8a2058785183e9bb83107e4cc5
aksupaul852131 <aksupaul.in@gmail.com>
2023-01-23 18:11:11 +0530

    add fw

--- src/components/app-banner.tsx
+++ src/components/app-banner.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const AppBanner = () => {
     return (
-        <Link href='' >
+        <Link href='https://play.google.com/store/apps/details?id=com.smartcare.supaul' >
             <img className="fixed z-20 bottom-0 md:hidden" src='/appbanner.png' />
         </Link>
     );
